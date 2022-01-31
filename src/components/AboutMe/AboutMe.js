e150f17f687322395ba924dd38d264b0532744d0
Vadzim Dzianisik <wadik.nhl@gmail.com>
2022-01-31 18:29:26 +0300

    add animations from react-typing-effect

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./AboutMe.module.scss";
 import common from "../common/common.module.scss";
 import photo from "./../../assets/img/me3.jpg"
+import ReactTypingEffect from 'react-typing-effect';
 
 export const AboutMe = () => (
   <section className={s.about_me}>
@@ -10,7 +11,11 @@ export const AboutMe = () => (
       <div className={s.content}>
         <div className={s.text}>
           <p>Hi, my name is<br/><span>Vadzim Dzianisik</span></p>
-          <p>and i'm a<br/><span>Front-end Developer</span></p>
+          <p>and i'm a<br/>
+            <span>
+              <ReactTypingEffect text="Front-end Developer"/>
+            </span>
+          </p>
         </div>
 
         <div className={s.picture}>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,23 +1,26 @@
 import React from "react";
 import s from './Nav.module.scss'
+import Fade from "react-reveal/Fade";
 
 export function Nav() {
   return (
     <nav className={s.nav}>
-      <ul className={s.items}>
-        <li className={s.item}>
-          <a className={s.link} href="#!">About me</a>
-        </li>
-        <li className={s.item}>
-          <a className={s.link} href="#!">Skills</a>
-        </li>
-        <li className={s.item}>
-          <a className={s.link} href="#!">Projects</a>
-        </li>
-        <li className={s.item}>
-          <a className={s.link} href="#!">Contacts</a>
-        </li>
-      </ul>
+      <Fade right>
+        <ul className={s.items}>
+          <li className={s.item}>
+            <a className={s.link} href="#!">About me</a>
+          </li>
+          <li className={s.item}>
+            <a className={s.link} href="#!">Skills</a>
+          </li>
+          <li className={s.item}>
+            <a className={s.link} href="#!">Projects</a>
+          </li>
+          <li className={s.item}>
+            <a className={s.link} href="#!">Contacts</a>
+          </li>
+        </ul>
+      </Fade>
     </nav>
   );
 }
